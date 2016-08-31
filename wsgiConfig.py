42bf01f0ab1767915702ce96191a98538ec21261
Felipe Hernández <cephei.1313@gmail.com>
2016-08-31 13:28:47 -0300

    refactoring

--- wsgiConfig.py
+++ wsgiConfig.py
@@ -2,22 +2,22 @@ import sys
 import os
 
 if len(sys.argv) < 2:
-	pass
+    pass
 else:
-	pathProject =  sys.argv[1]
-	FILE = open(pathProject + '/server/server/wsgi.py','r')
+    pathProject =  sys.argv[1]
+    FILE = open(pathProject + '/server/server/wsgi.py','r')
  
-	newLine = []
+    newLine = []
  
-	for line in FILE:
- 		if "sys.path.append" in line:
-   			newLine.append('sys.path.append(\'' + pathProject + '/server\')\n')
-  		else:
-   			newLine.append(line)
+    for line in FILE:
+         if "sys.path.append" in line:
+               newLine.append('sys.path.append(\'' + pathProject + '/server\')\n')
+          else:
+               newLine.append(line)
 
-	FILE.close()
+    FILE.close()
 
-	FILE = open(pathProject + '/server/server/wsgi.py','w')
-	for line in newLine:
-		FILE.write(line)
-	FILE.close()
+    FILE = open(pathProject + '/server/server/wsgi.py','w')
+    for line in newLine:
+        FILE.write(line)
+    FILE.close()
